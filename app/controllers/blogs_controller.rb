6ea68e926603c6c468e71fab118262b625ea2b1a
Haruka Teduka <th_kiyose@outlook.com>
2020-05-25 13:57:40 +0900

    投稿機能を作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,23 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all.order(created_at: :DESC)
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+      flash[:notice] = "投稿が完了しました。"
+    else
+      render :new
+    end
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:content).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only:[:new,:create,:destroy]
-  resources :blogs
+  resources :blogs, only:[:index,:new,:create]
   resources :users, only: [:new,:create,:show]
   root to: "blogs#index"
 end
