5ed21a1f08ffa76a36e5ffe7fd6acc8053bc79ac
Haruka Teduka <th_kiyose@outlook.com>
2020-05-26 11:21:11 +0900

    エラー画面の作成

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,6 @@
 module BlogsHelper
   def confirm_or_not
-    if action_name == "new"
+    if action_name == "new" || action_name == "create" || action_name == "confirm"
       confirm_blogs_path
     end
   end
